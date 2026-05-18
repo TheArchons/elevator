9d77a2d0ada71b104c3e272bd51dabf9d186d697
TheArchons <github@thearchons.xyz>
2026-05-18 12:22:18 -0700

    complete challenge 1

--- elevator.js
+++ elevator.js
@@ -2,32 +2,41 @@
     /*
         Algorithm description
 
+
         Init:
-        All elevators are going up (since they start at the ground floor)
+            All elevators are going up (since they start at the ground floor)
+
 
         Down Floor Button:
+            If a down elevator is already scheduled to go to this floor, ignore it
+            if there is an idle elevator, make idle elevator closest to this floor go to this floor (and remove it from the idle set)
+            Else find the lowest elevator above this floor that is also going down, and add it to its queue
 
-        If a down elevator is already scheduled to go to this floor, ignore it
-        if there is an idle elevator, make idle elevator closest to this floor go to this floor (and remove it from the idle set)
-        Else find the lowest elevator above this floor that is also going down, and add it to its queue
 
         Similarly for an Up Floor Button:
+            If a up elevator is already scheduled to go to this floor, ignore it
+            if there is an idle elevator, make idle elevator closest to this floor go to this floor (and remove it from the idle set)
+            Else find the highest elevator below this floor that is also going up, and add it to its queue
 
-        If a up elevator is already scheduled to go to this floor, ignore it
-        if there is an idle elevator, make idle elevator closest to this floor go to this floor (and remove it from the idle set)
-        Else find the highest elevator below this floor that is also going up, and add it to its queue
+            Note that in both cases we can ignore the case where there are no valid elevators to add, since the user will repress the butotn later anyway
 
-        Note that in both cases we can ignore the case where there are no valid elevators to add, since the user will repress the butotn later anyway
 
         Floor Button Pressed:
+            Assume that the user does not press a floor that is not in the direction of the elevator.
 
-        Assume that the user does not press a floor that is not in the direction of the elevator.
+            Insert the floor into the queue.
 
-        Insert the floor into the queue.
 
         Idle:
+            Add it to a set of idle elevators so it can be used
 
-        Add it to a set of idle elevators so it can be used
+
+        At floor 0
+            Set indicator to go up since that's the only way to go
+
+
+        At highest floor:
+            Set indicator to go down since that's the only way to go
     */
     init: function(es, fs) {
         function getScheduledFloors(isUp) {
@@ -45,7 +54,7 @@
             // inserts instead of appends
             e.addToQueue =(fNum) => {
                 e.destinationQueue.push(fNum);
-                e.destinationQueue.sort((a, b) => a - b)
+                e.destinationQueue.sort((a, b) => e.goingUpIndicator() ? a - b : b - a)
                 e.checkDestinationQueue();
             }
             e.goingDownIndicator(false);
@@ -57,6 +66,16 @@
 
             e.on("idle", () => idleEs.add(e));
 
+            e.on("stopped_at_floor", (floorNum) => {
+                if (floorNum === 0) {
+                    e.goingUpIndicator(true);
+                    e.goingDownIndicator(false);
+                } else if (floorNum === fs.length - 1) {
+                    // max floor
+                    e.goingUpIndicator(false);
+                    e.goingDownIndicator(true);
+                }
+            })
         });
 
         fs.map(f => {
